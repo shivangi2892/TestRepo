5d70c4e7e3d9af02bfce56ed09e0d14ce7438e1a
shivangi2892 <93086756+shivangi2892@users.noreply.github.com>
2022-10-22 20:35:55 +0530

    Update Chapter_01.c

--- Report-1/Chapter_01.c
+++ Report-1/Chapter_01.c
@@ -2,15 +2,13 @@
 main()
 {
     int n,sum=0;
-
-
      do
      {
          printf("Enter number");
          scanf("%d",&n);
          sum+=n;
-
-
      }while(sum<=100);
+    
+    printf("Sum is %d",sum);
 
 }
